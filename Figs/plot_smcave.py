b5f879f05579a47453ac120ade947884ab3f853b
Karl D. Gordon <kgordon@stsci.edu>
2024-05-07 15:04:56 -0400

    reversing previous commit

--- Figs/plot_smcave.py
+++ Figs/plot_smcave.py
@@ -15,7 +15,9 @@ from measure_extinction.extdata import ExtData, AverageExtData
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--bumps", help="save figure as a png file", action="store_true")    
+    parser.add_argument(
+        "--bumps", help="save figure as a png file", action="store_true"
+    )
     parser.add_argument("--png", help="save figure as a png file", action="store_true")
     parser.add_argument("--pdf", help="save figure as a pdf file", action="store_true")
     args = parser.parse_args()
@@ -70,7 +72,7 @@ if __name__ == "__main__":
             ctable["WAVELENGTH"] = np.copy(text.waves[uvtype].to(u.Angstrom))
             ctable["FLUX"] = np.copy(text.exts[uvtype])
             ctable["STAT-ERROR"] = np.copy(text.uncs[uvtype])
-            ctable["NPTS"] = np.full(len(text.waves[uvtype]), 1.)
+            ctable["NPTS"] = np.full(len(text.waves[uvtype]), 1.0)
 
             ntable = merge_stis_obsspec([ctable])
             next = copy.deepcopy(text)
@@ -104,25 +106,42 @@ if __name__ == "__main__":
     aveext.calc_AV_JHK()
     rv = aveext.columns["AV"][0]
     aveext.columns["RV"] = aveext.columns.pop("AV")
-    #aveext.exts["BAND"] = aveext.exts["BAND"] / rv + 1
-    #aveext.exts["STIS"] = aveext.exts["STIS"] / rv + 1
+    # aveext.exts["BAND"] = aveext.exts["BAND"] / rv + 1
+    # aveext.exts["STIS"] = aveext.exts["STIS"] / rv + 1
 
     if not args.bumps:
-        ax.plot(G03_SMCBar.obsdata_x, (G03_SMCBar.obsdata_axav - 1)*G03_SMCBar.Rv, 'ko',
-            label="G03 SMCBar; $R(V) = 2.74$")
+        ax.plot(
+            G03_SMCBar.obsdata_x,
+            (G03_SMCBar.obsdata_axav - 1) * G03_SMCBar.Rv,
+            "ko",
+            label="G03 SMCBar; $R(V) = 2.74$",
+        )
     aveext.save(ofilename)
 
     mod_x = np.arange(0.5, 9.0, 0.1)
     mwmod = G23(Rv=rv)
     mod_y = (mwmod(mod_x) - 1) * 3.1
-    ax.plot(mod_x, mod_y, "g--", alpha=0.75, label=f"Milky Way: G23 R(V)={rv:.2f}", linewidth=3.0)
+    ax.plot(
+        mod_x,
+        mod_y,
+        "g--",
+        alpha=0.75,
+        label=f"Milky Way: G23 R(V)={rv:.2f}",
+        linewidth=3.0,
+    )
 
-    aveext.plot(ax, color="b", rebin_fac=rebinfac, wavenum=True, legend_key="STIS",
-                legend_label=f"{klabel}: R(V) = {rv:.2f}")
+    aveext.plot(
+        ax,
+        color="b",
+        rebin_fac=rebinfac,
+        wavenum=True,
+        legend_key="STIS",
+        legend_label=f"{klabel}: R(V) = {rv:.2f}",
+    )
 
     # rebin to a constant resolution for the tables
     # save to a simple data file: useful for DGFit
-    aveext.rebin_constres("STIS", np.array([1100., 3200.]) * u.AA, 25)
+    aveext.rebin_constres("STIS", np.array([1100.0, 3200.0]) * u.AA, 25)
     waves = []
     exts = []
     uncs = []
@@ -133,20 +152,21 @@ if __name__ == "__main__":
         exts = np.concatenate((exts, aveext.exts[ckey][gvals]))
         uncs = np.concatenate((uncs, aveext.uncs[ckey][gvals]))
         if ckey == "BAND":
-            otypes = np.concatenate((otypes,  np.array(aveext.names[ckey])[gvals]))
+            otypes = np.concatenate((otypes, np.array(aveext.names[ckey])[gvals]))
         else:
             otypes = np.concatenate((otypes, np.full(np.sum(gvals), "spec")))
 
+    # not doing this as then the FM90 fits are inconsistent.  Just explaining this in the paper
     # get the V band value to allow for explicit normalization to V band for A(l)/A(V)
     # not exactly normalized as the E(B-V) values are those from the fits, so the
     # E(lambda-V)/E(B-V) curves are such that V in not exactly 0 and B is not exactly 1
-    sindxs = np.argsort(np.absolute(waves - 0.55 * u.micron))
-    actual_av = (exts[sindxs[0]] / rv) + 1.0
+    # sindxs = np.argsort(np.absolute(waves - 0.55 * u.micron))
+    # actual_av = (exts[sindxs[0]] / rv) + 1.0
 
     dgtab = QTable()
     sindxs = np.argsort(waves)
     dgtab["wave"] = waves[sindxs]
-    dgtab["A(l)/A(V)"] = ((exts[sindxs] / rv) + 1.0) / actual_av
+    dgtab["A(l)/A(V)"] = (exts[sindxs] / rv) + 1.0
     dgtab["unc"] = uncs[sindxs] / rv
     dgtab["type"] = otypes[sindxs]
     if args.bumps:
